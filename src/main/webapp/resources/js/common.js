2fa9f1bfdde16e3ee0fab27a0360d4df981b5ef2
Wayne Zheng <ellis.zheng@gmail.com>
2016-03-07 17:31:32 +0800

    会员级别设定

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -1,3 +1,67 @@
+var messages = {
+    "admin.message.success": "操作成功",
+    "admin.message.error": "操作错误",
+    "admin.dialog.ok": "确&nbsp;&nbsp;定",
+    "admin.dialog.cancel": "取&nbsp;&nbsp;消",
+    "admin.dialog.deleteConfirm": "您确定要删除吗？",
+    "admin.dialog.clearConfirm": "您确定要清空吗？",
+    "admin.browser.title": "选择文件",
+    "admin.browser.upload": "本地上传",
+    "admin.browser.parent": "上级目录",
+    "admin.browser.orderType": "排序方式",
+    "admin.browser.name": "名称",
+    "admin.browser.size": "大小",
+    "admin.browser.type": "类型",
+    "admin.browser.select": "选择文件",
+    "admin.upload.sizeInvalid": "上传文件大小超出限制",
+    "admin.upload.typeInvalid": "上传文件格式不正确",
+    "admin.upload.invalid": "上传文件格式或大小不正确",
+    "admin.validate.required": "必填",
+    "admin.validate.email": "E-mail格式错误",
+    "admin.validate.url": "网址格式错误",
+    "admin.validate.date": "日期格式错误",
+    "admin.validate.dateISO": "日期格式错误",
+    "admin.validate.pointcard": "信用卡格式错误",
+    "admin.validate.number": "只允许输入数字",
+    "admin.validate.digits": "只允许输入零或正整数",
+    "admin.validate.minlength": "长度不允许小于{0}",
+    "admin.validate.maxlength": "长度不允许大于{0}",
+    "admin.validate.rangelength": "长度必须在{0}-{1}之间",
+    "admin.validate.min": "不允许小于{0}",
+    "admin.validate.max": "不允许大于{0}",
+    "admin.validate.range": "必须在{0}-{1}之间",
+    "admin.validate.accept": "输入后缀错误",
+    "admin.validate.equalTo": "两次输入不一致",
+    "admin.validate.remote": "输入错误",
+    "admin.validate.integer": "只允许输入整数",
+    "admin.validate.positive": "只允许输入正数",
+    "admin.validate.negative": "只允许输入负数",
+    "admin.validate.decimal": "数值超出了允许范围",
+    "admin.validate.pattern": "格式错误",
+    "admin.validate.extension": "文件格式错误"
+};
+
+function message(code) {
+    if (code != null) {
+        var content = messages[code] != null ? messages[code] : code;
+        if (arguments.length == 1) {
+            return content;
+        } else {
+            if ($.isArray(arguments[1])) {
+                $.each(arguments[1], function (i, n) {
+                    content = content.replace(new RegExp("\\{" + i + "\\}", "g"), n);
+                });
+                return content;
+            } else {
+                $.each(Array.prototype.slice.apply(arguments).slice(1), function (i, n) {
+                    content = content.replace(new RegExp("\\{" + i + "\\}", "g"), n);
+                });
+                return content;
+            }
+        }
+    }
+}
+
 // 添加Cookie
 function addCookie(name, value, options) {
     if (arguments.length > 1 && name != null) {
