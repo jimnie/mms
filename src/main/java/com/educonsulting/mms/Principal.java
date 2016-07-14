7d5ba78667cb0c32309d221a71a220c9243d19f1
Wayne Zheng <ellis.zheng@gmail.com>
2016-07-14 15:38:23 +0800

    全部使用long作为id

--- src/main/java/com/educonsulting/mms/Principal.java
+++ src/main/java/com/educonsulting/mms/Principal.java
@@ -6,20 +6,20 @@ public class Principal implements Serializable {
 
     private static final long serialVersionUID = 5798882004228239559L;
 
-    private Long id;
+    private String id;
 
     private String username;
 
-    public Principal(Long id, String username) {
+    public Principal(String id, String username) {
         this.id = id;
         this.username = username;
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
