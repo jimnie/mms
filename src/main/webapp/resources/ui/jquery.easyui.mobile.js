9bfdd6e1e175238ecd24606b9c8ab140bb13f9a3
jimnie <ellis.zheng@outlook.com>
2018-04-22 12:38:10 +0800

    easyui 1.5.4.5

--- src/main/webapp/resources/ui/jquery.easyui.mobile.js
+++ src/main/webapp/resources/ui/jquery.easyui.mobile.js
@@ -1,7 +1,7 @@
-﻿/**
- * jQuery EasyUI 1.5
+/**
+ * EasyUI for jQuery 1.5.4.5
  * 
- * Copyright (c) 2009-2016 www.jeasyui.com. All rights reserved.
+ * Copyright (c) 2009-2018 www.jeasyui.com. All rights reserved.
  *
  * Licensed under the freeware license: http://www.jeasyui.com/license_freeware.php
  * To use it on other terms please contact us: info@jeasyui.com
@@ -124,18 +124,18 @@ $.mobile._go(_16,_17,_18);
 },back:function(){
 history.go(-1);
 }};
-    $.map(["validatebox", "textbox", "passwordbox", "filebox", "searchbox", "combo", "combobox", "combogrid", "combotree", "combotreegrid", "datebox", "datetimebox", "numberbox", "spinner", "numberspinner", "timespinner", "datetimespinner"], function (_19) {
+$.map(["validatebox","textbox","passwordbox","filebox","searchbox","combo","combobox","combogrid","combotree","combotreegrid","datebox","datetimebox","numberbox","spinner","numberspinner","timespinner","datetimespinner"],function(_19){
 if($.fn[_19]){
-    $.extend($.fn[_19].defaults, {iconWidth: 28, tipPosition: "bottom"});
+$.extend($.fn[_19].defaults,{iconWidth:28,tipPosition:"bottom"});
 }
 });
 $.map(["spinner","numberspinner","timespinner","datetimespinner"],function(_1a){
-    if ($.fn[_1a]) {
-        $.extend($.fn[_1a].defaults, {iconWidth: 56, spinAlign: "horizontal"});
-    }
+if($.fn[_1a]){
+$.extend($.fn[_1a].defaults,{iconWidth:56,spinAlign:"horizontal"});
+}
 });
-    if ($.fn.menu) {
+if($.fn.menu){
 $.extend($.fn.menu.defaults,{itemHeight:30,noline:true});
-    }
+}
 })(jQuery);
 
