4abbc2ecc1761357729ef1e176722e7fb0808fb5
jimnie <ellis.zheng@outlook.com>
2019-04-01 12:05:03 +0800

    货架编号信息

---
+++ src/main/java/com/educonsulting/mms/controller/ShelfController.java
@@ -0,0 +1,41 @@
+package com.educonsulting.mms.controller;
+
+import com.educonsulting.mms.entity.Shelf;
+import com.educonsulting.mms.service.ShelfService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
+@RequestMapping("/shelf")
+public class ShelfController extends BaseController {
+
+    @Resource(name = "shelfServiceImpl")
+    private ShelfService shelfService;
+
+    @RequestMapping(value = "/idel", method = RequestMethod.GET)
+    @ResponseBody
+    public Object findIdelShelf() {
+        List<Shelf> shelfList = shelfService.findIdelShelf(0);
+        return shelfList.get(0);
+    }
+
+    @RequestMapping(value = "/usableShelf", method = RequestMethod.GET)
+    @ResponseBody
+    public Object findUsableShelf(@RequestParam(value = "code", required = false) String code) {
+        Shelf shelf = shelfService.findShelfByCode(code);
+        return shelf;
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public Object list() {
+        List<Shelf> shelfList = shelfService.findIdelShelf(0);
+        return shelfList;
+    }
+}
