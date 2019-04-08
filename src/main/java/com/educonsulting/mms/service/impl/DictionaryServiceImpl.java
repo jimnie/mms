822aa341b59aee0058a6df77cf9fcd494b8e12eb
jimnie <ellis.zheng@outlook.com>
2019-04-08 15:55:33 +0800

    返回字典值对应的名称

--- src/main/java/com/educonsulting/mms/service/impl/DictionaryServiceImpl.java
+++ src/main/java/com/educonsulting/mms/service/impl/DictionaryServiceImpl.java
@@ -91,15 +91,4 @@ public class DictionaryServiceImpl extends BaseServiceImpl<Dictionary, Long> imp
         super.save(entity);
     }
 
-    @Override
-    public String findDictNameByValue(String fieldName, String value) {
-        List<Dictionary> dList = dictionaryDao.findDict(fieldName);
-        String name = null;
-        for (Dictionary dictionary : dList) {
-            if (dictionary.getValue().equals(value)) {
-                name = dictionary.getName();
-            }
-        }
-        return name;
-    }
 }
