cc8a40e5633fa9af4a96fe3f6403f3f88b80bb02
jimnie <ellis.zheng@outlook.com>
2019-04-10 17:18:00 +0800

    pdf文件名称

--- src/main/java/com/educonsulting/mms/util/Constants.java
+++ src/main/java/com/educonsulting/mms/util/Constants.java
@@ -4,5 +4,22 @@ package com.educonsulting.mms.util;
  * Created by Wayne on 2016/6/30.
  */
 public class Constants {
+
     public static final String CURRENT_USER = "user";
+
+    public static final String JASPER_FILE_PATH = "WEB-INF\\jasper\\";
+
+    public static final String GENERATE_PDF_PATH = "pdf\\";
+
+    public static final String GENERATE_PIC_PATH = "sign\\";
+
+    public static final String PIC_TYPE_JPG = ".jpg";
+
+    public static final String DEPOSIT_INVOICE = "\\1.骨灰盒寄存单.pdf";
+
+    public static final String PLACE_SERVICE = "\\2.骨灰装置服务确认书.pdf";
+
+    public static final String DRAW_CERT = "\\3.领取业务证明单.pdf";
+
+    public static final String GIVEUP_CONFIRM = "\\放弃骨灰确认书.pdf";
 }
