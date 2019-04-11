3c4c5b6d9f7eb488b59562034af82ece7c7b3edb
jimnie <ellis.zheng@outlook.com>
2019-04-11 17:58:41 +0800

    jasper编译文件

--- src/main/java/com/educonsulting/mms/entity/Draw.java
+++ src/main/java/com/educonsulting/mms/entity/Draw.java
@@ -1,7 +1,11 @@
 package com.educonsulting.mms.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
@@ -10,15 +14,27 @@ public class Draw extends BaseEntity {
 
     private static final long serialVersionUID = 6396453961861884792L;
 
-    private String serivceNo;
+    private String serviceNo;
+
+    private String dpName;
+
+    private Integer dpSex;
+
+    private Integer dpAge;
+
+    private Integer dpCertType;
+
+    private String dpCertNo;
 
     private String utName;
 
     private Integer utCertType;
 
+    private String utCertNo;
+
     private String phone;
 
-    private String utCertNo;
+    private Integer drawType;
 
     private Date drawDate;
     // 收回；丢失；
@@ -28,18 +44,26 @@ public class Draw extends BaseEntity {
 
     private String memo;
 
-    private Handover handover;
+    private Long hId;
 
-    private Deposit deposit;
+    private Long dId;
 
-    public String getSerivceNo() {
-        return serivceNo;
+    private String signPic;
+    // 领取经办人
+    private String createdBy;
+
+    @NotNull
+    @Column(name = "service_no", nullable = false, unique = true, length = 11)
+    public String getServiceNo() {
+        return serviceNo;
     }
 
-    public void setSerivceNo(String serivceNo) {
-        this.serivceNo = serivceNo;
+    public void setServiceNo(String serviceNo) {
+        this.serviceNo = serviceNo;
     }
 
+    @NotNull
+    @Column(name = "ut_name", nullable = false, length = 50)
     public String getUtName() {
         return utName;
     }
@@ -48,6 +72,8 @@ public class Draw extends BaseEntity {
         this.utName = utName;
     }
 
+    @NotNull
+    @Column(name = "ut_cert_type")
     public Integer getUtCertType() {
         return utCertType;
     }
@@ -56,6 +82,8 @@ public class Draw extends BaseEntity {
         this.utCertType = utCertType;
     }
 
+    @NotNull
+    @Column(name = "phone", nullable = false, length = 20)
     public String getPhone() {
         return phone;
     }
@@ -64,6 +92,8 @@ public class Draw extends BaseEntity {
         this.phone = phone;
     }
 
+    @NotNull
+    @Column(name = "ut_cert_no", nullable = false, length = 18)
     public String getUtCertNo() {
         return utCertNo;
     }
@@ -72,6 +102,9 @@ public class Draw extends BaseEntity {
         this.utCertNo = utCertNo;
     }
 
+    @NotNull
+    @Column(name = "draw_date", nullable = false, columnDefinition = "date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", locale = "zh")
     public Date getDrawDate() {
         return drawDate;
     }
@@ -80,6 +113,8 @@ public class Draw extends BaseEntity {
         this.drawDate = drawDate;
     }
 
+    @NotNull
+    @Column(name = "invoice_state")
     public Integer getInvoiceState() {
         return invoiceState;
     }
@@ -88,6 +123,8 @@ public class Draw extends BaseEntity {
         this.invoiceState = invoiceState;
     }
 
+    @NotNull
+    @Column(name = "magcard_state")
     public Integer getMagcardState() {
         return magcardState;
     }
@@ -96,6 +133,7 @@ public class Draw extends BaseEntity {
         this.magcardState = magcardState;
     }
 
+    @Column(name = "memo", columnDefinition = "text")
     public String getMemo() {
         return memo;
     }
@@ -104,19 +142,101 @@ public class Draw extends BaseEntity {
         this.memo = memo;
     }
 
-    public Handover getHandover() {
-        return handover;
+    @Column(name = "h_id")
+    public Long gethId() {
+        return hId;
     }
 
-    public void setHandover(Handover handover) {
-        this.handover = handover;
+    public void sethId(Long hId) {
+        this.hId = hId;
     }
 
-    public Deposit getDeposit() {
-        return deposit;
+    @Column(name = "d_id")
+    public Long getdId() {
+        return dId;
     }
 
-    public void setDeposit(Deposit deposit) {
-        this.deposit = deposit;
+    public void setdId(Long dId) {
+        this.dId = dId;
+    }
+
+    @NotNull
+    @Column(name = "sign_pic", nullable = false, length = 50)
+    public String getSignPic() {
+        return signPic;
+    }
+
+    public void setSignPic(String signPic) {
+        this.signPic = signPic;
+    }
+
+    @NotNull
+    @Column(name = "dp_name", nullable = false, length = 50)
+    public String getDpName() {
+        return dpName;
+    }
+
+    public void setDpName(String dpName) {
+        this.dpName = dpName;
+    }
+
+    @NotNull
+    @Column(name = "dp_sex")
+    public Integer getDpSex() {
+        return dpSex;
+    }
+
+    public void setDpSex(Integer dpSex) {
+        this.dpSex = dpSex;
+    }
+
+    @NotNull
+    @Column(name = "dp_cert_type")
+    public Integer getDpCertType() {
+        return dpCertType;
+    }
+
+    public void setDpCertType(Integer dpCertType) {
+        this.dpCertType = dpCertType;
+    }
+
+    @NotNull
+    @Column(name = "dp_cert_no", nullable = false, length = 18)
+    public String getDpCertNo() {
+        return dpCertNo;
+    }
+
+    public void setDpCertNo(String dpCertNo) {
+        this.dpCertNo = dpCertNo;
+    }
+
+    @NotNull
+    @Column(name = "dp_draw_type")
+    public Integer getDrawType() {
+        return drawType;
+    }
+
+    public void setDrawType(Integer drawType) {
+        this.drawType = drawType;
+    }
+
+    @NotNull
+    @Column(name = "dp_age")
+    public Integer getDpAge() {
+        return dpAge;
+    }
+
+    public void setDpAge(Integer dpAge) {
+        this.dpAge = dpAge;
+    }
+
+    @NotNull
+    @Column(name = "created_by", nullable = false, length = 20)
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
     }
 }
