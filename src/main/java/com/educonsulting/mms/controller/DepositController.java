27582e3469ec2b446f3ab3d4a923c895ba154c74
jimnie <ellis.zheng@outlook.com>
2019-04-11 17:06:09 +0800

    更新

--- src/main/java/com/educonsulting/mms/controller/DepositController.java
+++ src/main/java/com/educonsulting/mms/controller/DepositController.java
@@ -7,13 +7,25 @@ import com.educonsulting.mms.Pageable;
 import com.educonsulting.mms.entity.Deposit;
 import com.educonsulting.mms.service.DepositService;
 import com.educonsulting.mms.service.UserService;
+import com.educonsulting.mms.util.Constants;
+import com.educonsulting.mms.util.ImageGenerator;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/deposit")
@@ -67,15 +79,28 @@ public class DepositController extends BaseController {
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     @ResponseBody
+    @Transactional
     public Message save(Deposit deposit) {
         if (depositService.isServiceNoExist(deposit.getServiceNo())) {
             return Message.error("服务编号已存在");
         }
+        String webRoot = System.getProperty("webapp.root");
         deposit.setStatus(0); // 收存状态为已收存
-        // TODO: 收存操作员
+        // 收存操作员
         deposit.setCreatedBy(userService.getCurrent().getName());
+
+        String fileName = UUID.randomUUID().toString() + Constants.PIC_TYPE_JPG;
+        String jpgPath = webRoot + Constants.GENERATE_PIC_PATH + fileName;
+        boolean succ = ImageGenerator.generateImage(deposit.getSignPic(), jpgPath);
+        deposit.setSignPic(fileName);
         // TODO: 领取操作员
         depositService.save(deposit);
+        // 按服务编号创建目录名称
+        String pdfPath = webRoot + Constants.GENERATE_PDF_PATH + deposit.getServiceNo();
+        File file = new File(pdfPath);
+        if (!file.exists()) {
+            file.mkdir();
+        }
         return SUCCESS_MESSAGE;
     }
 
@@ -92,4 +117,28 @@ public class DepositController extends BaseController {
         List<Deposit> list = depositService.findDepositByServiceNo(serviceNo);
         return list;
     }
+
+    @RequestMapping(value = "/viewPDF/{serviceNo}", method = RequestMethod.GET, produces = "application/pdf")
+    public void viewPdf(HttpServletRequest request,
+                        HttpServletResponse response,
+                        @PathVariable String serviceNo) {
+        String pdfPath = depositService.buildPdf(serviceNo);
+        String webRoot = System.getProperty("webapp.root");
+        Path file = Paths.get(webRoot + Constants.GENERATE_PDF_PATH + serviceNo + "\\", Constants.DEPOSIT_INVOICE_PDF);
+        try {
+            if (Files.exists(file)) {
+                response.setContentType("application/pdf");
+                // 浏览器内打开
+                response.addHeader("Content-Disposition", "inline; filename=" + URLEncoder.encode(serviceNo + "." + Constants.DEPOSIT_INVOICE_PDF, "utf-8").replaceAll("\\+", "%20"));
+                // 浏览器内下载
+//                response.addHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode(serviceNo + "." + Constants.DEPOSIT_INVOICE_PDF, "utf-8").replaceAll("\\+", "%20"));
+
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
