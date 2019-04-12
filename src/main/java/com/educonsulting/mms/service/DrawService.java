073dee15b2dc5d5cd4d7c210f338757c1bc2f45b
jimnie <ellis.zheng@outlook.com>
2019-04-12 12:00:23 +0800

    临时提交

--- src/main/java/com/educonsulting/mms/service/DrawService.java
+++ src/main/java/com/educonsulting/mms/service/DrawService.java
@@ -1,6 +1,18 @@
 package com.educonsulting.mms.service;
 
+import com.educonsulting.mms.Page;
+import com.educonsulting.mms.Pageable;
 import com.educonsulting.mms.entity.Draw;
 
+import java.util.List;
+
 public interface DrawService extends BaseService<Draw, Long> {
+
+    boolean isServiceNoExist(String serviceNo);
+
+    Page<Draw> findpage(Pageable pageable);
+
+    String buildPdf(String serviceNo, String fileName, String type);
+
+    public List<Draw> findDepositByServiceNo(String serviceNo);
 }
